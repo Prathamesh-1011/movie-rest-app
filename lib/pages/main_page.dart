a3daa205600fafd94935afef7380db078c6d2a67
Prathamesh Rokade <prathameshr955@gmail.com>
2024-03-21 21:27:30 +0530

    creating main page background UI

--- lib/pages/main_page.dart
+++ lib/pages/main_page.dart
@@ -1,13 +1,56 @@
+// ignore_for_file: prefer_const_constructors
+
+import 'dart:ui';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
 class MainPage extends ConsumerWidget {
+  double? _deviceHeight;
+  double? _deviceWidth;
+
   @override
   Widget build(BuildContext context, WidgetRef ref) {
+    _deviceHeight = MediaQuery.of(context).size.height;
+    _deviceWidth = MediaQuery.of(context).size.width;
     return _buildUI();
   }
 
   Widget _buildUI() {
-    return Scaffold();
+    return Scaffold(
+      backgroundColor: Colors.black,
+      body: Container(
+        height: _deviceHeight,
+        width: _deviceWidth,
+        child: Stack(
+          alignment: Alignment.center,
+          children: [
+            _backgroundWidget(),
+          ],
+        ),
+      ),
+    );
+  }
+
+  Widget _backgroundWidget() {
+    return Container(
+      height: _deviceHeight,
+      width: _deviceWidth,
+      decoration: BoxDecoration(
+        borderRadius: BorderRadius.circular(10.0),
+        image: DecorationImage(
+          image: NetworkImage(
+              'https://images-na.ssl-images-amazon.com/images/I/91B32iU7ayL._AC_SL1500_.jpg'),
+          fit: BoxFit.cover,
+        ),
+      ),
+      child: BackdropFilter(
+        filter: ImageFilter.blur(sigmaX: 15.0, sigmaY: 15.0),
+        child: Container(
+          decoration: BoxDecoration(
+            color: Colors.black.withOpacity(0.2),
+          ),
+        ),
+      ),
+    );
   }
 }
